6d71fab8d0ffe124611f277f7e448742c660d16f
Alexander040904 <jonathanalexanderramirezortega@gmail.com>
2024-12-12 04:31:10 -0600

    🦄 refactor(navigation, dashbord): Nav responsive
    Se actualido el nav-responsive para que funcionara con secciones

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -24,71 +24,89 @@
 
     </x-app-layout>
 @else
-    @section('nav-link')
-        <x-nav-link :href="route('activation', 1)" :active="request()->query('source') === '1'">
-            {{ __('Activos') }}
-        </x-nav-link>
-
-        <x-nav-link :href="route('activation', 2)" :active="request()->query('source') === '2'">
-            {{ __('Inactivos') }}
-        </x-nav-link>
-    @endsection
-    <x-app-layout>
-        <x-slot name="header">
-            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                {{ __('Alumnos ') }}
-            </h2>
-        </x-slot>
-
-        <x-content-box>
-            @if (session('activeUsers') && count(session('activeUsers')) > 0)
-
-                @foreach (session('activeUsers') as $user)
-                    <!-- Mostrar propiedades de cada usuario en 'activeUsers' -->
-
-                    <div class="p-6 text-gray-900 dark:text-gray-100">
-                        <div class="flex justify-between items-center">
-                            <!-- Nombre del usuario -->
-                            <div>
-                                {{ $user->name }}
-                            </div>
-                            <!-- Botón -->
-                            <div>
-                                @if (request()->query('source') === '1')
-                                    <form action="{{ route('deactivate', $user->id) }}" method="POST">
-                                        @csrf
-                                        @method('PUT')
-                                        <x-danger-button>
-                                            Desactivar
-                                        </x-danger-button>
 
 
-                                    </form>
-                                @else
-                                    <form action="{{ route('activate', $user->id) }}" method="POST">
-                                        @csrf
-                                        @method('PUT')
-                                        <!-- Esto indica que la solicitud debe ser de tipo PUT -->
-                                        <x-danger-button>
-                                            Activar
-                                        </x-danger-button>
-                                    </form>
-                                @endif
+        @section('nav-link')
+            <x-nav-link :href="route('activation', 1)" :active="request()->query('source') === '1'">
+                {{ __('Activos') }}
+            </x-nav-link>
 
+            <x-nav-link :href="route('activation', 2)" :active="request()->query('source') === '2'">
+                {{ __('Inactivos') }}
+            </x-nav-link>
+            <x-nav-link :href="route('activation', 2)" :active="request()->query('source') === '2'">
+                {{ __('Crear carrera') }}
+            </x-nav-link>
+        @endsection
+
+        @section('nav-responsive')
+            <x-responsive-nav-link :href="route('activation', 1)" :active="request()->query('source') === '1'">
+                {{ __('Activos') }}
+            </x-responsive-nav-link>
+
+            <x-responsive-nav-link :href="route('activation', 2)" :active="request()->query('source') === '2'">
+                {{ __('Inactivos') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('activation', 2)" :active="request()->query('source') === '2'">
+                {{ __('Crear carrera') }}
+            </x-responsive-nav-link>
+        @endsection
+
+
+
+        <x-app-layout>
+            <x-slot name="header">
+                <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                    {{ __('Alumnos ') }}
+                </h2>
+            </x-slot>
+
+            <x-content-box>
+                @if (session('activeUsers') && count(session('activeUsers')) > 0)
+
+                    @foreach (session('activeUsers') as $user)
+                        <!-- Mostrar propiedades de cada usuario en 'activeUsers' -->
+
+                        <div class="p-6 text-gray-900 dark:text-gray-100">
+                            <div class="flex justify-between items-center">
+                                <!-- Nombre del usuario -->
+                                <div>
+                                    {{ $user->name }}
+                                </div>
+                                <!-- Botón -->
+                                <div>
+                                    @if (request()->query('source') === '1')
+                                        <form action="{{ route('deactivate', $user->id) }}" method="POST">
+                                            @csrf
+                                            @method('PUT')
+                                            <x-danger-button>
+                                                Desactivar
+                                            </x-danger-button>
+
+
+                                        </form>
+                                    @else
+                                        <form action="{{ route('activate', $user->id) }}" method="POST">
+                                            @csrf
+                                            @method('PUT')
+                                            <!-- Esto indica que la solicitud debe ser de tipo PUT -->
+                                            <x-danger-button>
+                                                Activar
+                                            </x-danger-button>
+                                        </form>
+                                    @endif
+
+                                </div>
                             </div>
                         </div>
-                    </div>
-                @endforeach
-            @else
-                {{ 'No hay nada' }}
+                    @endforeach
+                @else
+                    {{ 'No hay nada' }}
 
 
-            @endif
-        </x-content-box>
+                @endif
+            </x-content-box>
 
-
-
-
-    </x-app-layout>
+        </x-app-layout>
 
 @endif
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -21,7 +21,7 @@
             </div>
 
             <!-- Settings Dropdown -->
-            <div class="hidden sm:flex sm:items-center sm:ms-6">
+            <div class="sm:flex sm:items-center sm:ms-6">
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button
@@ -75,35 +75,15 @@
     </div>
 
     <!-- Responsive Navigation Menu -->
-    <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
-        </div>
+    <div :class="{ 'block': open, 'hidden': !open }" class="open sm:hidden">
 
         <!-- Responsive Settings Options -->
-        <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
-            <div class="px-4">
-                <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
-                <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
-            </div>
+        <div class=" pb-1 border-t border-gray-200 dark:border-gray-600">
+
 
             <div class="mt-3 space-y-1">
-                <x-responsive-nav-link :href="route('profile.edit')">
-                    {{ __('Profile') }}
-                </x-responsive-nav-link>
+                @yield('nav-responsive')
 
-                <!-- Authentication -->
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-
-                    <x-responsive-nav-link :href="route('logout')"
-                        onclick="event.preventDefault();
-                                        this.closest('form').submit();">
-                        {{ __('Log Out') }}
-                    </x-responsive-nav-link>
-                </form>
             </div>
         </div>
     </div>
